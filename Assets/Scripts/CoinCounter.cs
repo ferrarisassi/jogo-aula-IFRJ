a79b2a0f449612ff45a2c3a23379ccd999c44d8e
Ferrari <54215984+ferrarisassi@users.noreply.github.com>
2025-10-31 10:39:31 -0300

    Moeda changes

--- Assets/Scripts/CoinCounter.cs
+++ Assets/Scripts/CoinCounter.cs
@@ -4,15 +4,12 @@ public class CoinCounter : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // This code will execute when another 2D collider enters this trigger
         Debug.Log("Trigger entered by: " + other.gameObject.name);
 
-        // You can use 'other' to access the GameObject that entered the trigger
         if (other.CompareTag("Player"))
         {
             Debug.Log("Player entered the trigger!");
-            // Perform actions specific to the player entering
+            Destroy(gameObject);
         }
-        Destroy(gameObject);
     }    
 }
