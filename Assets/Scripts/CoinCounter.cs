e2796020e9c3bd1431b87e2743188b1ce0d0e8b3
Ferrari <54215984+ferrarisassi@users.noreply.github.com>
2025-11-14 10:36:13 -0300

    Moeda Bug Fix and Death Zone

--- Assets/Scripts/CoinCounter.cs
+++ Assets/Scripts/CoinCounter.cs
@@ -4,11 +4,11 @@ public class CoinCounter : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Trigger entered by: " + other.gameObject.name);
+        //Debug.Log("Trigger entered by: " + other.gameObject.name);
 
         if (other.CompareTag("Player"))
         {
-            Debug.Log("Player entered the trigger!");
+            //Debug.Log("Player entered the trigger!");
             Destroy(gameObject);
         }
     }    
